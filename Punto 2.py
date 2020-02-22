46e0d4bb98dcbe1feeb9f731823e0db72a6e00a9
Hiver77 <iver.hincapie@gmail.com>
2020-02-22 00:00:39 -0500

    Final

--- Punto 2.py
+++ Punto 2.py
@@ -18,21 +18,21 @@ class Polinomio:
 
     def Resta(self, Pol):
         
-        coef_r  = [a-b for a,b in zip(self.Coeficientes, Pol.Coeficiente)]
+        coef_r  = [a-b for a,b in zip(self.Coeficientes, Pol.Coeficientes)]
         if len(self.Coeficientes) < len(Pol.Coeficientes):
             coef_r+= [-v for v in Pol.Coeficientes[len(self.Coeficientes):]]
         else: 
-            coef_r+=self.Coeficientes[len(Pol.Coeficientes)]
+            coef_r+=self.Coeficientes[len(Pol.Coeficientes):]
 
         return Polinomio(coef_r)
 
 
     def Multi(self,Pol):
 
-        r= [0]*(len(self.Coeficientes)+len(Pol.Coeficientes)-1)
+        coef_r = [0]*(len(self.Coeficientes)+len(Pol.Coeficientes)-1)
         for sgrado, scoef in enumerate(self.Coeficientes):
             for grado, coef in enumerate(Pol.Coeficientes):
-                coef_r[sgrado+ogrado]+= scoef*coef
+                coef_r[sgrado+grado]+= scoef*coef
 
         return Polinomio(coef_r)
 
@@ -42,6 +42,12 @@ class Polinomio:
 
     def evaluar(self,num):
 
+        r= 0
+        for c in reversed(self.Coeficientes):
+            r*= num
+            r+= c
+        return r
+
 
 
 
@@ -50,4 +56,9 @@ Pol1  = Polinomio([1,2,3])
 Pol2 = Polinomio([2,3,4])
 
 a = Pol1.Suma(Pol2)
-print(a.Coeficientes)
+b= Pol1.Resta(Pol2)
+c= Pol1.Multi(Pol2)
+d=Pol2.escalar(8)
+e=Pol2.evaluar(1)
+
+print(a.Coeficientes,b.Coeficientes,c.Coeficientes,d.Coeficientes,e)
